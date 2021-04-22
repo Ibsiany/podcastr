63a4dff46307b97aabcfa03cb367fd4588ac020f
ibsiany <ibsianyd@gmail.com>
2021-04-22 20:11:33 -0300

    aula 4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 import styles from './home.module.scss';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 interface Episode {
   id: string;
@@ -13,7 +15,7 @@ interface Episode {
   thumbnail: string;
   members: string;
   publishedAt: string;
-  duration: string;
+  duration: number;
   durationAsString: string;
   url: string;
 }
@@ -24,10 +26,12 @@ interface HomeProps {
 }
 
 export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
+  const {play} = useContext(PlayerContext);
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
-        <h2>Últimos lançamentos</h2>
+        <h2>Últimos lançamentos </h2>
         <ul>
           {latestEpisodes.map(episode => {
             return(
@@ -50,7 +54,7 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio"/>
                 </button>
               </li>
