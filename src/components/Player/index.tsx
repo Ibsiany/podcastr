3217710ccdf5d47b94d99d4443799e8ac5187173
ibsiany <ibsianyd@gmail.com>
2021-04-23 21:04:11 -0300

    aula 05

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -13,7 +13,11 @@ export function Player(){
         currentEpisodeIndex, 
         isPlaying, 
         togglePlay,
-        setPlayingState
+        setPlayingState,
+        playNext,
+        playPrevious,
+        hasPrevious,
+        hasNext
     } = useContext(PlayerContext);
 
     useEffect(()=> {
@@ -86,7 +90,7 @@ export function Player(){
                     <button type="button"disabled={!episode}>
                         <img src="/shuffle.svg" alt="Embaralhar"/>
                     </button>
-                    <button type="button" disabled={!episode}>
+                    <button type="button" disabled={!episode || !hasPrevious} onClick={playPrevious}>
                         <img src="/play-previous.svg" alt="Tocar anterior"/>
                     </button>
                     <button 
@@ -97,7 +101,7 @@ export function Player(){
                     >
                         { isPlaying ? <img src="/pause.svg" alt="Tocar"/> : <img src="/play.svg" alt="Tocar"/>}
                     </button>
-                    <button type="button" disabled={!episode}>
+                    <button type="button" disabled={!episode || !hasNext} onClick={playNext}>
                         <img src="/play-next.svg" alt="Tocar próxima"/>
                     </button>
                     <button type="button" disabled={!episode}>
---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,90 @@
+import {createContext, ReactNode, useState} from 'react';
+
+interface Episode {
+    title: string;
+    members: string;
+    thumbnail: string;
+    duration: number;
+    url: string;
+}
+
+interface PlayerContextData {
+    episodeList: Array<Episode>;
+    currentEpisodeIndex: number;
+    isPlaying: boolean;
+    play: (episode: Episode) => void;
+    playlist: (list: Episode[], index: number) => void;
+    togglePlay: () => void;
+    setPlayingState: (state: boolean) => void;
+    playNext: () => void;
+    playPrevious: () => void;
+    hasPrevious: boolean;
+    hasNext: boolean;
+}
+
+export const PlayerContext = createContext({} as PlayerContextData)
+
+interface PlayerContextProviderProps {
+    children: ReactNode;
+}
+
+export function PlayerContextProvider({children}: PlayerContextProviderProps){
+    const [episodeList, setEpisodeList] = useState([]);
+    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+    const [isPlaying, setIsPlaying] = useState(false)
+  
+  
+    function play(episode: Episode){
+      setEpisodeList([episode])
+      setCurrentEpisodeIndex(0)
+      setIsPlaying(true)
+    }
+
+    function playlist(list: Episode[], index: number){
+        setEpisodeList(list);
+        setCurrentEpisodeIndex(0)
+        setIsPlaying(true)
+    }
+  
+    function togglePlay(){
+      setIsPlaying(!isPlaying)
+    }
+  
+    function setPlayingState(state: boolean){
+      setIsPlaying(state)
+    }
+
+    const hasPrevious = currentEpisodeIndex > 0;
+    const hasNext = (currentEpisodeIndex + 1) < episodeList.length;
+
+    function playNext(){
+        if(hasNext){
+            setCurrentEpisodeIndex(currentEpisodeIndex + 1)        
+        }
+    }
+
+    function playPrevious(){
+        if (hasPrevious){
+            setCurrentEpisodeIndex(currentEpisodeIndex - 1)
+        }
+    }
+  
+    return (
+        <PlayerContext.Provider 
+            value={{
+                episodeList, 
+                currentEpisodeIndex, 
+                play, 
+                playlist,
+                isPlaying,
+                togglePlay, 
+                setPlayingState,
+                playNext,
+                playPrevious,
+                hasNext,
+                hasPrevious
+            }}>
+            {children}
+        </PlayerContext.Provider>
+    )
+}
